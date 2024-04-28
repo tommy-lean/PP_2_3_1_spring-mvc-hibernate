08e69b12e5b2c9c99430c357b0eae6bbd40ea843
tommy-lean <k-tomilin99@mail.ru>
2024-04-29 00:03:51 +0500

    task complete

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -46,5 +46,6 @@ public class WebConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+        resolver.setCharacterEncoding("UTF-8");
     }
 }
